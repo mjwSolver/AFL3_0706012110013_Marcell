34c022961e96c52c3195536bb6496f3c45719a15
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-14 09:18:25 +0700

    Added new Landmark

---
+++ AFL3_0706012110013_Marcell/Landmark.swift
@@ -0,0 +1,16 @@
+//
+//  Landmark.swift
+//  AFL3_0706012110013_Marcell
+//
+//  Created by MacBook Pro on 14/04/23.
+//
+
+import Foundation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+}
