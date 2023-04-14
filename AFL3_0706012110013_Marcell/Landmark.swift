50977323033fde751b54c9f13b624df9db397df7
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-14 09:22:00 +0700

    Further updating Landmark

--- AFL3_0706012110013_Marcell/Landmark.swift
+++ AFL3_0706012110013_Marcell/Landmark.swift
@@ -6,11 +6,27 @@
 //
 
 import Foundation
+import SwiftUI
 
+// Defining the Landmark Struct
 struct Landmark: Hashable, Codable {
     var id: Int
     var name: String
     var park: String
     var state: String
     var description: String
+    
+    // Inserting Code to store the Image
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    // For storing coordinate informations
+    private var coordinates: Coordinates
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
 }
