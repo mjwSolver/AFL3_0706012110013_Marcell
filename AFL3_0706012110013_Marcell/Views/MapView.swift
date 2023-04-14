0f79ddf79c4f68bd60b0b7215dc558e514e00f9c
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-14 10:06:15 +0700

    View setOnAppear

--- AFL3_0706012110013_Marcell/Views/MapView.swift
+++ AFL3_0706012110013_Marcell/Views/MapView.swift
@@ -11,14 +11,21 @@ import MapKit
 // Struct to define location coordinates
 struct MapView: View {
     var coordinate: CLLocationCoordinate2D
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-    )
+    @State private var region = MKCoordinateRegion()
 
     // body to generate map view
     var body: some View {
         Map(coordinateRegion: $region)
+            .onAppear {
+                setRegion(coordinate)
+            }
+    }
+    
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
     }
 }
 
