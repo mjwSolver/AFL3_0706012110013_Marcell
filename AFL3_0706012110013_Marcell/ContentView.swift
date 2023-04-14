b1d1aadc9a6df976ff71104a70e744317c499a8d
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-14 09:02:23 +0700

    Insert CircleImage and Comments

--- AFL3_0706012110013_Marcell/ContentView.swift
+++ AFL3_0706012110013_Marcell/ContentView.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        // Create a new VStack to organize the view
         VStack {
+            // Initialize the Mapview
             MapView()
                 .frame(height: 300)
+            
+            // Insert the precreated Image
+            // Change it's positioning
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
             
             VStack {
                 VStack(alignment: .leading) {
