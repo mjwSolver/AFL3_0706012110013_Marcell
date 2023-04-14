adb726882c94b412a815f24f5aa5a03357ef0aac
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-14 10:13:55 +0700

    More Previews

--- AFL3_0706012110013_Marcell/Views/LandmarkList.swift
+++ AFL3_0706012110013_Marcell/Views/LandmarkList.swift
@@ -28,7 +28,10 @@ struct LandmarkList: View {
 // Previewing as iPhone SE
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
-              .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+           LandmarkList()
+               .previewDevice(PreviewDevice(rawValue: deviceName))
+               .previewDisplayName(deviceName)
+       }
     }
 }
