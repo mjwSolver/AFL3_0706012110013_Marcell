fa4746a8eb2473c493143247d485ea035ea7fbc3
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-21 21:55:03 +0700

    Creating CategoryItem

---
+++ AFL3_0706012110013_Marcell/Views/CategoryItem.swift
@@ -0,0 +1,33 @@
+//
+//  CategoryItem.swift
+//  AFL3_0706012110013_Marcell
+//
+//  Created by MacBook Pro on 21/04/23.
+//
+
+import SwiftUI
+
+// Holds Landmark as a struct containing all the details.
+struct CategoryItem: View {
+    var landmark: Landmark
+
+    // Displaying Name and Modded Imaged
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+// temporarily displaying information
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem(landmark: ModelData().landmarks[0])
+    }
+}
