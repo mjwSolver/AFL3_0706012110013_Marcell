a14a758a027f1d331e7c96eea95e032d1e07aa95
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-21 21:51:57 +0700

    Displaying Category Details

--- AFL3_0706012110013_Marcell/Views/CategoryRow.swift
+++ AFL3_0706012110013_Marcell/Views/CategoryRow.swift
@@ -13,7 +13,21 @@ struct CategoryRow: View {
     var items: [Landmark]
 
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        Text(landmark.name)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
     }
 }
 
@@ -24,7 +38,7 @@ struct CategoryRow_Previews: PreviewProvider {
     static var previews: some View {
         CategoryRow(
             categoryName: landmarks[0].category.rawValue,
-            items: Array(landmarks.prefix(3))
+            items: Array(landmarks.prefix(4))
         )
     }
 }
