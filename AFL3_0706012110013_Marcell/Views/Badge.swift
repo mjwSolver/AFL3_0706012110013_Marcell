0bcd7476144d519c4c99c5150bb83c2c89143264
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-21 19:39:44 +0700

    Create multiple symbols

--- AFL3_0706012110013_Marcell/Views/Badge.swift
+++ AFL3_0706012110013_Marcell/Views/Badge.swift
@@ -10,9 +10,15 @@ import SwiftUI
 struct Badge: View {
     // Create the Badge Symbol
     var badgeSymbols: some View {
-            RotatedBadgeSymbol(angle: Angle(degrees: 0))
-                .opacity(0.5)
+        // Create multiple instances of the symbol
+        // At different Angles
+        ForEach(0..<8) { index in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(index) / Double(8)) * 360.0
+            )
         }
+        .opacity(0.5)
+    }
     
     // Use Z stack to sandwich each foreground and background.
     var body: some View {
