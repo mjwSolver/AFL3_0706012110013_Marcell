86bcfb1eed0a6ecb2ae7a2a083a21988ba69fa91
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-25 15:58:02 +0700

    EditMode as a shared property value

--- AFL3_0706012110013_Marcell/Views/Profiles/ProfileHost.swift
+++ AFL3_0706012110013_Marcell/Views/Profiles/ProfileHost.swift
@@ -9,17 +9,23 @@ import SwiftUI
 
 // The Profile Host will hold the state and final display
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
     @State private var draftProfile = Profile.default
 
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                Spacer()
+                EditButton()
+            }
+            
             ProfileSummary(profile: draftProfile)
         }
         .padding()
     }
 }
 
-// Without adding the envinronment object, the child view will fail to load. 
+// Without adding the envinronment object, the child view will fail to load.
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
