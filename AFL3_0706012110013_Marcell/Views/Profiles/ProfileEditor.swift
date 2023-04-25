b580e94f94a392fd36c9478a38d83a1da7c9fb29
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-25 16:33:25 +0700

    Creating an Editor View

---
+++ AFL3_0706012110013_Marcell/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileEditor.swift
+//  AFL3_0706012110013_Marcell
+//
+//  Created by MacBook Pro on 25/04/23.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    @Binding var profile: Profile
+    
+    var body: some View {
+        List {
+            HStack {
+                Text("Username").bold()
+                Divider()
+                TextField("Username", text: $profile.username)
+            }
+        }
+    }
+}
+
+struct ProfileEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditor(profile: .constant(.default))
+    }
+}
