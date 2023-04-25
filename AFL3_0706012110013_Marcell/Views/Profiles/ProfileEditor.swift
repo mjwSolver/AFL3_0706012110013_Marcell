62e4e20d10a48f7350716e404c6fc629b2b9154d
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-25 16:40:27 +0700

    Selecting Seasonal Photos

--- AFL3_0706012110013_Marcell/Views/Profiles/ProfileEditor.swift
+++ AFL3_0706012110013_Marcell/Views/Profiles/ProfileEditor.swift
@@ -18,9 +18,22 @@ struct ProfileEditor: View {
                 TextField("Username", text: $profile.username)
             }
             
+            // Toggle UI based on the users saved preferences
            Toggle(isOn: $profile.prefersNotifications) {
                Text("Enable Notifications").bold()
            }
+            
+            // UI for selecting which seasonal photo
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Seasonal Photo").bold()
+                
+                Picker("Seasonal Photo", selection: $profile.seasonalPhoto) {
+                    ForEach(Profile.Season.allCases) { season in
+                        Text(season.rawValue).tag(season)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
         }
     }
 }
