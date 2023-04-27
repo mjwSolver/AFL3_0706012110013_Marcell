ac6668787e78466c3159b80f83a076148670102e
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-27 10:28:34 +0700

    MacLandmark

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by MacBook Pro on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
