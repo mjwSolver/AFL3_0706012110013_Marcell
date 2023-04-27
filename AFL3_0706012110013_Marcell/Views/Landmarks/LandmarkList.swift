5ea412507235fb78d3aaef5181e40fad1555b794
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-27 20:25:49 +0700

    Updating LandmarkList
    Adding CategoryEnum
    Creating adaptive Title
    Adding Toolbar with favorites inside for toggling

--- AFL3_0706012110013_Marcell/Views/Landmarks/LandmarkList.swift
+++ AFL3_0706012110013_Marcell/Views/Landmarks/LandmarkList.swift
@@ -12,13 +12,33 @@ import SwiftUI
 struct LandmarkList: View {
     @EnvironmentObject var modelData: ModelData
     @State private var showFavoritesOnly = false
+    @State private var filter = FilterCategory.all
+    
+    // Creating a filter category
+    enum FilterCategory: String, CaseIterable, Identifiable {
+        case all = "All"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+
+        var id: FilterCategory { self }
+    }
+
     
     var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
+            // Additional filtering logic
+                && (filter == .all || filter.rawValue == landmark.category.rawValue)
         }
     }
 
+    // Title changes if we're viewing favorites
+    var title: String {
+        let title = filter == .all ? "Landmarks" : filter.rawValue
+        return showFavoritesOnly ? "Favorite \(title)" : title
+    }
+    
     var body: some View {
         NavigationView {
             List {
@@ -32,12 +52,22 @@ struct LandmarkList: View {
                 }
             }
         }
-        .navigationTitle("Landmarks")
+        .navigationTitle(title)
         .frame(minWidth: 300)
         // Add a toolbar, inside it is a toggle for favorite.
         .toolbar {
             ToolbarItem {
                 Menu {
+                    // Adding a picker for selecting the category
+                    Picker("Category", selection: $filter) {
+                            ForEach(FilterCategory.allCases) { category in
+                                Text(category.rawValue).tag(category)
+                            }
+                        }
+                        .pickerStyle(.inline)
+
+
+                    
                     Toggle(isOn: $showFavoritesOnly) {
                         Label("Favorites only", systemImage: "star.fill")
                     }
@@ -45,6 +75,8 @@ struct LandmarkList: View {
                     Label("Filter", systemImage: "slider.horizontal.3")
                 }
             }
+            
+            Text("Select a Landmark")
         }
     }
 }
