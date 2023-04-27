89e974bd165fce940120ebbb351d914b34679905
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-27 20:29:01 +0700

    Creating LandmarkCommands
    It's in iOS and also macOS

---
+++ AFL3_0706012110013_Marcell/Views/Landmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  AFL3_0706012110013_Marcell
+//
+//  Created by MacBook Pro on 27/04/23.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
